9d13efa48d3566c8cf920840710ad9f4df2bc3ca
Phạm Văn Nguyên <npv2k1@gmail.com>
2022-03-23 21:04:44 +0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import VirtualList from '@sveltejs/svelte-virtual-list';
+  let emojis = []
+  for (let i=128100;i<128293;i++){
+    emojis.push(String.fromCodePoint(`${i}`.toString(16)))  
+   
+  }
+  // console.log(emojis)
+  let start;
+	let end;
+</script>
+
+<div class="container">
+<!--   {#each emojis as emoji}
+  <p class="text">{emoji}</p>
+{/each} -->
+
+  <VirtualList {emojis} bind:start bind:end let:item>
+		<p class="text">{item}</p>
+	</VirtualList>
+</div>
+
+<style>
+  .text {
+    font-size: 25px;
+  }
+  .container {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+  }
+</style>
